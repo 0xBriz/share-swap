20670c6a375be5ca6b303a93b0c88997660b438f
0xBriz <98397602+0xBriz@users.noreply.github.com>
2022-04-24 20:36:25 -0400

    removed logs

--- contracts/ShareSwap.sol
+++ contracts/ShareSwap.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "hardhat/console.sol"; // TODO: not prod
-
 contract ShareSwap is Ownable {
     using SafeERC20 for IERC20;
 
